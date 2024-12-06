19a8bb8330a93ba7536bc93c83f1073ca562a55e
İpek Nas <ipeknnas@gmail.com>
2024-12-06 09:21:41 +0300

    a

---
+++ hafta11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>readme oluşturma</title>
+</head>
+<body>
+<?php
+    $filename = "readme.txt"
+    $file fopen($filename,"w")
+    echo"readme!!";
+    fclose($file)
+?>    
+</body>
+</html>
---
+++ phpintro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sıralı Sayılar</title>
+</head>
+<body>
+
+
+<?php
+    echo"hi";
+    for ($i = 1;$i <= 100; $i++){
+        if($i %2 !=0)
+        echo $i ." ";
+    } 
+?>    
+</body>
+</html>
